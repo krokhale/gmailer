ef2be455ebd67b28570c52c4a6d0b4fd9d151e32
Jonathan E Hogue <jon@hogue.org>
2010-11-16 04:37:48 +0000

    Added a 2 test test script

--- test/test_gmailer.rb
+++ test/test_gmailer.rb
@@ -1,7 +1,16 @@
+#!/usr/bin/ruby
 require 'helper'
 
-class TestGmailer < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+class TestGmailer < Test::Unit::TestCase 
+  def test_new 
+    gmailer = Gmailer::Gmail.new( 'jon.hogue@gmail.com', 'C@rrR0b3' )
+    assert_kind_of gmailer, 'Gmailer'
   end
+  
+  def test_default_settingsg
+    gmailer = Gmailer::Gmail.new( 'jon.hogue@gmail.com', 'C@rrR0b3' )
+    assert( gmailer.success )
+    assert( gmailer.current_folder eq "inbox" )
+  end
+   
 end
