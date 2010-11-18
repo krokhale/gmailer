fb59f575c2cb2f54dd871c3d660a775f578394fa
krokhale <krishna.rokhale@gmail.com>
2010-11-17 22:36:04 -0500

    tests added

--- test/test_gmailer.rb
+++ test/test_gmailer.rb
@@ -1,23 +1,53 @@
+require 'rubygems'
 require "../lib/gmailer"
 require "test/unit"
 
+USR = ''
+PWD = ''
+LABEL = ''
+
 class TestGmailer < Test::Unit::TestCase 
   
-  @gmailer = Gmailer::Gmail.new( 'jon.hogue@gmail.com', 'C@rrR0b3' )
   
   def test_new 
-    assert_kind_of(@gmailer, 'Gmailer')
+    @gmailer = Gmailer::Gmail.login(USR, PWD)
+    assert(@gmailer.class == Gmailer::Gmail)
   end
   
   def test_default_settings
+    @gmailer = Gmailer::Gmail.login(USR, PWD)
     assert(@gmailer.success)
-    assert(@gmailer.current_folder eq "inbox")
+    assert(@gmailer.current_folder.match("inbox"))
   end
   
   def test_change_label
-    response  = true
+    @gmailer = Gmailer::Gmail.login(USR, PWD)
+    assert(@gmailer.current_folder.match("inbox"))
+    response = @gmailer.label(LABEL)
+    assert(response.match(/Success/))
+    assert(@gmailer.current_folder.match(LABEL))
+  end
+  
+  def test_order_by_date
+    @gmailer = Gmailer::Gmail.login(USR, PWD)
+    assert(@gmailer.current_order.match("ascending"))
+    @gmailer.order_by_date("descending")
+    assert(@gmailer.current_order.match("ascending"))
+  end
+  
+  def test_get_next_message
+    @gmailer = Gmailer::Gmail.login(USR, PWD)
+    assert(@gmailer.current_order.match("ascending"))
+    message1 = @gmailer.get_next_message
+    message2 = @gmailer.get_next_message
+    assert(message1.date < message2.date)
+    @gmailer.order_by_date("descending")
+    message3 = @gmailer.get_next_message
+    message4 = @gmailer.get_next_message
+    assert(message3.date > message4.date)   
   end
     
-   
+    
+    
 end
 
